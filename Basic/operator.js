51d965b9888a68cf8d0bee520f8e192d29cfb57d
utkarsh537 <utkarshmahichsbhu@gmail.com>
2025-07-01 18:28:09 +0000

    Conversion of data type

---
+++ Basic/operator.js
@@ -0,0 +1,16 @@
+
+let score = 33
+console.log(typeof score)
+
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+//true=1 false=0;
+//""= false ,   "value"=true
+
+let someNumber = 333
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
