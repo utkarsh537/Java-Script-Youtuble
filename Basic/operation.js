e04afdc1eb6b8c32ac8358619c2e83d59c43f0f2
utkarsh537 <utkarshmahichsbhu@gmail.com>
2025-07-01 18:46:21 +0000

    operation idea

---
+++ Basic/operation.js
@@ -0,0 +1,16 @@
+
+let str1= "utkarsh"
+let str2= "pandey"
+
+let str3 = str2 + str2
+console.log(str3)
+
+
+console.log("1"+2)
+console.log("1"+"2")
+console.log(1+"2")
+console.log(1+2+2+"2")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
