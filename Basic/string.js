822ec46f05886b1ff0fd8ffb88a4c49bb15664b9
Utkarsh Pandey <utkarshmahichsbhu@gmail.com>
2025-07-03 18:56:38 +0000

    explaination of array and its methods

--- Basic/string.js
+++ Basic/string.js
@@ -7,3 +7,13 @@ let sent = `this is template literal`
 console.log(sent)
 console.log(typeof sent)
 
+let str1 = "Utkarsh Kumar   "
+console.log(str1.toUpperCase())
+console.log(str1.toLowerCase())
+console.log(str1.trim())
+
+console.log(str1.slice(0,9))
+
+let str2 = "Pandey"
+let str3 = str1.concat(str2)
+console.log(str3)
