4f36d276b20ab30a3fb5775945f08e09225e6703
Utkarsh Pandey <utkarshmahichsbhu@gmail.com>
2025-07-10 08:59:33 +0000

    concept of date in JS

---
+++ Basic/date.js
@@ -0,0 +1,12 @@
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toDateString())
+
+let myCreatedDate = new Date (2025,10,7)
+console.log(myCreatedDate.toDateString)
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+
